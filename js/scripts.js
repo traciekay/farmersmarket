ff188d3971c8ffc9418d3589892a98be8c1c757c
traciekay <tkaonje@gmail.com>
2017-05-12 14:48:06 +0300

    new file

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $("#AddFarmer").click(function(event){
+      $(".newProduce").show();
+    });
+
+    $("#marketplace form").click(function(event) {
+      var farmInput= $("input#farmProduct").val();
+      var priceInput= $("input#price").val();
+      var quantityInput= $("input#quantity").val();
+      // var imageInput = $("input#image").val();
+
+      var new_row = "<tr><td>" + farmInput + "</td><td>" + priceInput + "</td><td>" + quantityInput +
+      // "</td><td>" + imageInput + "</td></tr>";
+    $("table tbody").append(new_row);
+    return false;
+
+
+
+      event.preventDefault();
+    });
+  });
