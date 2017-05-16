6a93de80d6f10db786e5006e682ca4c34da9b2bf
traciekay <tkaonje@gmail.com>
2017-05-16 20:54:18 +0300

    adds items

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,24 @@ $(document).ready(function() {
       $(".newProduce").show();
     });
 
-    $("#marketplace form").click(function(event) {
+    $("#marketplace").click(function(event) {
+
       var farmInput= $("input#farmProduct").val();
       var priceInput= $("input#price").val();
       var quantityInput= $("input#quantity").val();
-      var imageInput = $("input#image").val();
-
+      var imageInput = $("#image").val();
+      console.log(farmInput);
+      console.log(priceInput);
+      console.log(quantityInput);
       var new_row = "<tr><td>" + farmInput + "</td><td>" + priceInput + "</td><td>" + quantityInput +
       "</td><td>" + imageInput + "</td></tr>";
-
+      console.log(new_row);
       $("table tbody").append(new_row);
       return false;
+      // $("#myModal").hide();
+      // $('#myModal').dialog("close");
 
       event.preventDefault();
+      $("#myModal").hide();
     });
   });
