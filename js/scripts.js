e169e94636142fda1bee8e7cc098975f4e13dd84
traciekay <tkaonje@gmail.com>
2017-05-17 09:51:40 +0300

    images is now working

--- js/scripts.js
+++ js/scripts.js
@@ -8,19 +8,14 @@ $(document).ready(function() {
       var farmInput= $("input#farmProduct").val();
       var priceInput= $("input#price").val();
       var quantityInput= $("input#quantity").val();
-      var imageInput = $("#image").val();
-      console.log(farmInput);
-      console.log(priceInput);
-      console.log(quantityInput);
-      var new_row = "<tr><td>" + farmInput + "</td><td>" + priceInput + "</td><td>" + quantityInput +
-      "</td><td>" + imageInput + "</td></tr>";
-      console.log(new_row);
-      $("table tbody").append(new_row);
-      return false;
-      // $("#myModal").hide();
-      // $('#myModal').dialog("close");
+      var imageInput = $("#image").val().replace(/.*\\(.*)/, '$1')
 
-      event.preventDefault();
-      $("#myModal").hide();
+      var new_row = "<tr><td>" + farmInput + "</td><td>" + priceInput + "</td><td>" + quantityInput +
+      "</td><td><img src='img/" + imageInput + "'></td></tr>";
+
+      $("table tbody").append(new_row);
+      $(".newProduce").hide();
+      return false;
+
     });
   });
